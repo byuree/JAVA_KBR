bbd8d283398b48bc1112463b2294f159dfeac821
byuree <stella0518@naver.com>
2023-04-25 17:36:52 +0900

    7일차

---
+++ human/src/Day07/Ex06_UpDownCasting/Student.java
@@ -0,0 +1,33 @@
+package Day07.Ex06_UpDownCasting;
+
+public class Student extends Person {
+	
+	
+	int grade;
+	String major;
+	
+	
+	// 알트 + 시프트 + S -> C
+	public Student() {
+		this("이름없음", 0, 0, "없음");
+		
+	}
+	public Student(String name, int age, int grade, String major) {
+		super(name, age);
+		this.grade = grade;
+		this.major = major;
+		
+	}
+	public String work() {
+		return "공부를 합니다.";
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "Student [grade=" + grade + ", major=" + major + "]";
+	}
+	
+	
+
+}
